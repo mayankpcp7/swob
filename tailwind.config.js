1caadf983caccc7c776695eb5766ea7eac087f70
tarunmalik330 <maliktarun2005@gmail.com>
2024-08-08 16:50:49 +0530

    change in comment bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -73,8 +73,8 @@ export default {
       },
       boxShadow: {
         "accordain-shadow": "0px 0px 6px 0px #00000012",
-        "comment-box":
-          "0px 0px 0px 0px #0000001A, -2px 5px 11px 0px #0000001A, -7px 18px 20px 0px #00000017, -16px 41px 26px 0px #0000000D, -29px 73px 31px 0px #00000003, -46px 114px 34px 0px #00000000",
+        // "comment-box":
+        //   "0px 0px 0px 0px #0000001A, -2px 5px 11px 0px #0000001A, -7px 18px 20px 0px #00000017, -16px 41px 26px 0px #0000000D, -29px 73px 31px 0px #00000003, -46px 114px 34px 0px #00000000",
       },
     },
   },
