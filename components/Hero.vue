b9f3be06065015f772e5d13d52c42cb74ea38bfa
mayankpcp7 <jariwalmayank123@gmail.com>
2024-08-08 16:32:41 +0530

    progress bar

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,21 +1,34 @@
 <template>
-    <section class="bg-yellow relative py-12 md:py-16 lg:py-20">
-        <div class="container max-w-[1060px] mx-auto px-4 md:px-3 ">
-            <div class="row flex-row pb-9 flex-wrap flex">
-                <div class="w-full md:w-7/12">
-                    <h1 class="font-semibold max-w-[530px] tracking-[-4%] text-off-black text-center px-4 sm:px-0 md:text-start mx-auto md:mx-0 mb-4 mb-md-4 leading-120 text-custom-xmd md:text-custom-4xl">
-                        Revolutioniere dein
-                        HR-Management</h1>
-                    <p
-                        class="text-gray font-normal leading-144 max-w-[383px] mb-8 mb-md-6 text-center mx-auto md:mx-0 md:text-start text-lg">
-                        Eine HR-Software für jede Unternehmensgröße. Bleib über unsere Einführung auf dem Laufenden.</p>
-                </div>
-                <div class="w-full md:w-5/12">
-                    <NuxtImg class="w-full mx-auto max-w-[343px] max-h-[376px] md:max-w-[429px] md:max-h-[590px]"
-                        src="/assets/images/webp/header-cartoon.webp" />
-                </div>
-            </div>
+  <section class="bg-yellow relative py-12 md:py-16 lg:py-20">
+    <div class="container max-w-[1060px] mx-auto px-4 md:px-3">
+      <div class="row flex-row pb-9 flex-wrap flex">
+        <div class="w-full md:w-7/12">
+          <h1
+            class="font-semibold max-w-[530px] tracking-[-4%] text-off-black text-center px-4 sm:px-0 md:text-start mx-auto md:mx-0 mb-4 mb-md-4 leading-120 text-custom-xmd md:text-custom-4xl"
+          >
+            Revolutioniere dein HR-Management
+          </h1>
+          <p
+            class="text-gray font-normal leading-144 max-w-[383px] mb-8 mb-md-6 text-center mx-auto md:mx-0 md:text-start text-lg"
+          >
+            Eine HR-Software für jede Unternehmensgröße. Bleib über unsere
+            Einführung auf dem Laufenden.
+          </p>
         </div>
-        <NuxtImg class="absolute bottom-0 w-full" width="1440" height="111" src="/assets/images/webp/header-bottom-layer.webp" alt="bottom-layer"/>
-    </section>
+        <div class="w-full md:w-5/12">
+          <NuxtImg
+            class="w-full mx-auto max-w-[343px] max-h-[376px] md:max-w-[429px] md:max-h-[590px]"
+            src="/assets/images/webp/header-cartoon.webp"
+          />
+        </div>
+      </div>
+    </div>
+    <NuxtImg
+      class="absolute bottom-0 w-full"
+      width="1440"
+      height="111"
+      src="/assets/images/webp/header-bottom-layer.webp"
+      alt="bottom-layer"
+    />
+  </section>
 </template>
