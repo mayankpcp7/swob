3e27dc60b6b0f39b0182e749840f0aacf48c0cff
mayankpcp7 <jariwalmayank123@gmail.com>
2024-08-10 10:13:19 +0530

    merge sections

--- components/ModrensteFunktionen.vue
+++ components/ModrensteFunktionen.vue
@@ -49,6 +49,12 @@ onUnmounted(() => {
     clearInterval(timerInterval);
   }
 });
+// --------ACCORDION---------
+const isOpen = ref(true);
+
+const toggleAccordion = () => {
+  isOpen.value = !isOpen.value;
+};
 </script>
 
 <template>
@@ -85,94 +91,102 @@ onUnmounted(() => {
           sollicitudin diam massa.
         </p>
         <div
-          class="flex flex-col w-full p-3 mt-7 mx-auto bg-white border border-border-white sm:p-5 max-w-[500px] rounded-custom-xl"
-        >
-          <div class="flex items-center justify-between">
-            <div class="flex items-center">
-              <Icons name="personendaten" />
-              <p class="ps-2 text-custom-xxs sm:text-base font-bold leading-3">
-                Personendaten
-              </p>
-            </div>
-            <Icons name="dropArrow" />
-          </div>
-          <div class="flex w-full pt-3 sm:pt-4">
-            <div class="flex flex-col">
-              <p class="mb-1 text-[7.46px] sm:text-xs font-bold text-off-black">
-                Vorname
-              </p>
-              <BaseInput placeholder="" />
-            </div>
-            <div class="flex flex-col ms-6">
-              <p class="mb-1 text-[7.46px] sm:text-xs font-bold text-off-black">
-                Nachname
-              </p>
-              <BaseInput placeholder="" />
-            </div>
-          </div>
-          <div class="flex flex-col w-full pt-4">
+    class="flex flex-col w-full p-3 mt-7 mx-auto bg-white border border-border-white sm:p-5 max-w-[500px] rounded-custom-xl"
+  >
+    <div class="flex items-center justify-between">
+      <div class="flex items-center">
+        <Icons name="personendaten" />
+        <p class="ps-2 text-custom-xxs sm:text-base font-bold leading-3">
+          Personendaten
+        </p>
+      </div>
+      <Icons
+        name="dropArrow"
+        @click="toggleAccordion"
+        :class="{ 'rotate-180': isOpen }"
+        class="cursor-pointer transition-transform duration-300"
+      />
+    </div>
+    <transition name="accordion">
+      <div v-if="isOpen" class="flex flex-col w-full pt-3 sm:pt-4">
+        <div class="flex w-full pt-3 sm:pt-4">
+          <div class="flex flex-col">
             <p class="mb-1 text-[7.46px] sm:text-xs font-bold text-off-black">
-              Geburtsdatum
+              Vorname
             </p>
-            <div
-              class="flex items-center w-full px-3 border rounded-md border-border-white"
-            >
-              <Icons name="gabrudatsum" />
-              <BaseInput
-                type="tel"
-                class="!w-full"
-                className="!border-none"
-                placeholder="--/--/--"
-              />
+            <BaseInput placeholder="" />
+          </div>
+          <div class="flex flex-col ms-6">
+            <p class="mb-1 text-[7.46px] sm:text-xs font-bold text-off-black">
+              Nachname
+            </p>
+            <BaseInput placeholder="" />
+          </div>
+        </div>
+        <div class="flex flex-col w-full pt-4">
+          <p class="mb-1 text-[7.46px] sm:text-xs font-bold text-off-black">
+            Geburtsdatum
+          </p>
+          <div
+            class="flex items-center w-full px-3 border rounded-md border-border-white"
+          >
+            <Icons name="gabrudatsum" />
+            <BaseInput
+              type="tel"
+              class="!w-full"
+              placeholder="--/--/--"
+            />
+          </div>
+        </div>
+        <div class="flex w-full pt-4">
+          <div class="flex flex-col w-full">
+            <p class="mb-1 text-[7.46px] sm:text-xs font-bold text-off-black">
+              Nationalität
+            </p>
+            <div class="relative flex w-full">
+              <select
+                class="w-full px-3 py-2.5 md:text-lg truncate pr-6 text-custom-xs border border-border-white rounded-md text-input-gray appearance-none focus:outline-none"
+              >
+                <option>Schweiz</option>
+                <option>Germany</option>
+                <option>Austria</option>
+              </select>
+              <span
+                class="absolute top-1/2 right-3 md:right-5 transform -translate-y-1/2 z-10 pointer-events-none"
+              >
+                <Icons
+                  class="cursor-pointer !max-w-2 sm:max-w-3.5"
+                  name="selectArrow"
+                />
+              </span>
             </div>
           </div>
-          <div class="flex w-full pt-4">
-            <div class="flex flex-col w-full">
-              <p class="mb-1 text-[7.46px] sm:text-xs font-bold text-off-black">
-                Nationalität
-              </p>
-              <div class="relative flex w-full">
-                <select
-                  class="w-full px-3 py-2.5 md:text-lg truncate pr-6 text-custom-xs border border-border-white rounded-md text-input-gray appearance-none focus:outline-none"
-                >
-                  <option>Schweiz</option>
-                  <option>Germany</option>
-                  <option>Austria</option>
-                </select>
-                <span
-                  class="absolute top-1/2 right-3 md:right-5 transform -translate-y-1/2 z-10 pointer-events-none"
-                >
-                  <Icons
-                    class="cursor-pointer !max-w-2 sm:max-w-3.5"
-                    name="selectArrow"
-                  />
-                </span>
-              </div>
-            </div>
-            <div class="flex flex-col w-full ms-[14px] md:ms-6">
-              <p class="mb-1 text-[7.46px] sm:text-xs font-bold text-off-black">
-                Aufenthaltsstatus
-              </p>
-              <div class="relative flex w-full">
-                <select
-                  class="w-full px-3 py-2.5 md:text-lg truncate pr-6 text-custom-xs border border-border-white rounded-md text-input-gray appearance-none focus:outline-none"
-                >
-                  <option>Aufenthaltsstatus</option>
-                  <option>Germany</option>
-                  <option>Austria</option>
-                </select>
-                <span
-                  class="absolute top-1/2 right-3 md:right-5 transform -translate-y-1/2 z-10 pointer-events-none"
-                >
-                  <Icons
-                    class="cursor-pointer !max-w-2 sm:max-w-3.5"
-                    name="selectArrow"
-                  />
-                </span>
-              </div>
+          <div class="flex flex-col w-full ms-[14px] md:ms-6">
+            <p class="mb-1 text-[7.46px] sm:text-xs font-bold text-off-black">
+              Aufenthaltsstatus
+            </p>
+            <div class="relative flex w-full">
+              <select
+                class="w-full px-3 py-2.5 md:text-lg truncate pr-6 text-custom-xs border border-border-white rounded-md text-input-gray appearance-none focus:outline-none"
+              >
+                <option>Aufenthaltsstatus</option>
+                <option>Germany</option>
+                <option>Austria</option>
+              </select>
+              <span
+                class="absolute top-1/2 right-3 md:right-5 transform -translate-y-1/2 z-10 pointer-events-none"
+              >
+                <Icons
+                  class="cursor-pointer !max-w-2 sm:max-w-3.5"
+                  name="selectArrow"
+                />
+              </span>
             </div>
           </div>
         </div>
+      </div>
+    </transition>
+  </div>
       </div>
       <div
         class="flex xl:w-5/12 w-full md:w-6/12 flex-col p-4 bg-yellow-gradient rounded-xl md:max-w-[528px] xl:px-12 lg:pt-12 lg:pb-7"
@@ -182,9 +196,9 @@ onUnmounted(() => {
         >
           <div class="flex items-center p-3">
             <Icons name="sixdots" />
-            <span
-              class="ms-2 w-5 h-5 border border-border-white rounded-md"
-            ></span>
+            <input type="checkbox"
+              class="ms-2 w-5 h-5 border  border-border-white rounded-md"
+            ></input>
             <p class="ps-2 me-4 text-sm font-medium text-black">
               API-Endpunkte entwickeln
             </p>
@@ -338,7 +352,7 @@ onUnmounted(() => {
                 <Icons class="ms-[2.5px] sm:ms-1 md:ms-2.5" name="note" />
               </div>
               <div
-                class="border border-border-white lg:p-3.5 rounded-custom-sm p-1 md:p-2 md:rounded-lg"
+                class="border border-border-white lg:p-3.5 rounded-custom-sm p-1 md:p-1 md:rounded-lg"
               >
                 <Icons name="profile" />
               </div>
@@ -403,3 +417,20 @@ onUnmounted(() => {
     </div>
   </div>
 </template>
+
+
+<style>
+.accordion-enter-active,
+.accordion-leave-active {
+  transition: max-height 0.3s ease;
+}
+.accordion-enter,
+.accordion-leave-to {
+  max-height: 0;
+  overflow: hidden;
+}
+
+.rotate-180 {
+  transform: rotate(180deg);
+}
+</style>
