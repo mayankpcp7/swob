1381cc3a34e4bae3687fa496fd27399536a1129f
tarunmalik330 <maliktarun2005@gmail.com>
2024-08-10 12:11:42 +0530

    change in slider-images height

--- components/Pretium.vue
+++ components/Pretium.vue
@@ -3,10 +3,10 @@
         <div class="container max-w-[1140px] ps-4 md:ps-3 mx-auto">
             <h1 class="text-light-black text-custom-xmd pe-8 md:pe-3 text-center md:text-start px-6 md:ps-0 lg:text-custom-xl max-w-[1035px] mb-16 md:mb-6 font-bold !leading-110">
                 Lorem ipsum dolor sit amet consectetur. Diam pretium nunc.</h1>
-            <div class="flex items-center xl:me-[-150px] gap-4 md:gap-8 overflow-x-scroll">
-                <NuxtImg class="w-[288px] h-[192px] md:w-[600px] rounded-md md:rounded-xl md:h-[400px] lg:w-[768px] lg:h-[542px]" width="768" height="542" src="/assets/images/webp/pretium-1.webp" />
-                <NuxtImg class="w-[308px] h-[192px] md:w-[600px] md:h-[400px] lg:w-[809px] lg:h-[542px]" width="809" height="542" src="/assets/images/webp/pretium-2.webp" />
-                <NuxtImg class="w-[289px] h-[192px] md:w-[600px] md:h-[400px] lg:w-[809px] lg:h-[542px]" width="809" height="542" src="/assets/images/webp/pretium-3.webp" />
+            <div class="flex items-center xl:me-[-150px] gap-4 md:gap-8 overflow-x-scroll lg:pt-[50px] lg:pb-[30px]">
+                <NuxtImg class="w-[288px] h-[192px] md:w-[600px] rounded-md md:rounded-xl md:h-[400px] lg:w-[768px] lg:h-[512px]" width="768" height="542" src="/assets/images/webp/pretium-1.webp" />
+                <NuxtImg class="w-[308px] h-[192px] md:w-[600px] md:h-[400px] lg:w-[809px] lg:h-[512px]" width="809" height="542" src="/assets/images/webp/pretium-2.webp" />
+                <NuxtImg class="w-[289px] h-[192px] md:w-[600px] md:h-[400px] lg:w-[809px] lg:h-[512px]" width="809" height="542" src="/assets/images/webp/pretium-3.webp" />
                 <NuxtImg class="w-[289px] h-[192px] md:w-[600px] md:h-[400px] lg:w-[729px] lg:h-[500px]" width="729" height="500" src="/assets/images/webp/pretium-4.webp" />
                 <NuxtImg class="w-[278px] h-[192px] md:w-[400px] md:h-[400px] lg:w-[500px] lg:h-[500px]" width="500" height="500" src="/assets/images/webp/pretium-5.webp" />
                 <NuxtImg class="w-[288px] h-[192px] md:w-[400px] md:h-[400px] lg:w-[500px] lg:h-[500px]" width="500" height="500" src="/assets/images/webp/pretium-5.webp" />
